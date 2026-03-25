32477ae0af04d1f712368167d5d0850e8da4e793
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2026-03-25 13:58:56 -0300

    generic map

--- linear_map.py
+++ linear_map.py
@@ -5,6 +5,10 @@ import cv2
 
 
 def linear_map(matrix: np.ndarray, img: np.ndarray):
+	"""
+		map each vector in the original image to its new position using matrix multiplication
+	"""
+
 	assert matrix.ndim == 2
 	assert img.ndim == 3
 
@@ -18,15 +22,41 @@ def linear_map(matrix: np.ndarray, img: np.ndarray):
 			if 0 <= new_coord[0] < img.shape[1] and 0 <= new_coord[1] < img.shape[0]:
 				new_img[int(new_coord[1])][int(new_coord[0])] = pixl
 
-	cv2.imshow('linear map', new_img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	return new_img
+
+
+def generic_map(f, img):
+	"""
+		map each vector in the original image to its new position using any spacial map
+	"""
+	assert img.ndim == 3
+
+	new_img = np.zeros(shape=img.shape, dtype='uint8')
+
+	for i, line in enumerate(img):
+		for p, pixl in enumerate(line):
+			# compute new coordinates
+			new_coord = f([p, i])
+			# assign to new img
+			if 0 <= new_coord[0] < img.shape[1] and 0 <= new_coord[1] < img.shape[0]:
+				new_img[int(new_coord[1])][int(new_coord[0])] = pixl
+
+	return new_img
 
 
 v: np.ndarray = cv2.imread('assets/gam.jpg')
-A = np.array([[0.5, -0.5], [0, 0.5]])
+A = np.array([[0.5, 0], [0.5, 0.5]])
 
 theta = -math.pi/8
 R = np.array([[math.cos(theta), math.sin(theta)], [-math.sin(theta), math.cos(theta)]])
 
-linear_map(R, v)
+
+def f(coord):
+	return [math.sin(coord[0])*1800, math.cos(coord[1])*800]
+
+
+new_img = linear_map(A, v)
+
+cv2.imshow('linear map', new_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
